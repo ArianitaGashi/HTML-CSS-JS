2b45409321e0fc392ef90fc116a2f4b5c6c59843
ArianitaGashi <arianita.gashi@digitalschool.tech>
2026-03-05 17:41:33 +0100

    hi

--- Moduli26/main.js
+++ Moduli26/main.js
@@ -1,1 +1,79 @@
-console.log("Helloooo")
+console.log("Helloooo");
+
+var x = 10;
+
+var value = "Digital School";
+
+var y = 30;
+
+console.log(x+y);
+
+console.log(x-y);
+
+console.log(x*y);
+
+console.log(x/y);
+
+var a = 99;
+
+console.log(a);
+
+a++;
+
+console.log(a);
+
+var b = 201;
+
+b--;
+
+console.log(b);
+
+var c = 500;
+
+c+=5;
+
+console.log(c)
+
+var d = 55;
+
+d-=10;
+
+console.log(d);
+
+var e = 354;
+
+e*=4;
+
+console.log(e);
+
+var f = 670;
+
+f/=5;
+
+console.log(f);
+
+
+var v = 100;
+
+var w = 50;
+
+// operatort e krahasimit
+
+console.log(v == w);
+
+console.log(v != w);
+
+console.log(v > w);
+
+console.log(v < w);
+
+var emri = "Rigon";
+var mbiemri = "Hodza";
+
+var fullName = emri +" "+mbiemri;
+
+console.log(fullName);
+
+console.log(`Hello from ${emri}`);
+
+
